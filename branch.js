fdf755f6a5ec7efe47c03af8d7a70ddb51580b87
Sanchit <sanchitym@gmail.com>
2025-09-10 21:11:53 +0530

    branch command

---
+++ branch.js
@@ -0,0 +1,17 @@
+<p> git branch = to know the working on which branch
+    
+    git checkout -b feature1 ---- to create new branch with name /  to switch that branch remove -b 
+
+    git checkout main --- switch to main branch
+
+    git branch -d branch name  -- to delete the branch
+
+
+    <p>   this for creating new branch in existing branch
+        - so have to on inside that branch where u gonna create another  branch
+        ex- git checkout feature1 -then
+
+
+    </p>
+
+</p>
